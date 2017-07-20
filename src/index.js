2706ac45dbf25d3e4428dd91dadec71e6ee1b73f
Alessio <alessio.d@gmail.com>
2017-07-20 12:07:21 +0200

    Basic setup

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import p5 from 'p5';
+import { Sketch } from './sketch';
+
+new p5(Sketch);
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    //hot: true,
+    contentBase: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
